5e45ca6274abb03cfc8e6d32e99260a38b9bfa10
Ross Lavery <rosslavery@gmail.com>
2023-02-07 11:25:11 -0500

    chore: new lib

---
+++ projects/searchkings/ngx-tel-input/src/lib/ngx-tel-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-tel-input',
+  template: `
+    <p>
+      ngx-tel-input works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxTelInputComponent {
+
+}
---
+++ projects/searchkings/ngx-tel-input/src/lib/ngx-tel-input.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxTelInputComponent } from './ngx-tel-input.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxTelInputComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxTelInputComponent
+  ]
+})
+export class NgxTelInputModule { }
